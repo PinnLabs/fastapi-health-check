739eeaa7a98768576709ed1a3e07b3a8b285969c
Felipe Castro <146021799+castroofelipee@users.noreply.github.com>
2026-04-13 17:42:29 -0300

    Export the public library API

---
+++ src/fastapi_health_check/__init__.py
@@ -0,0 +1,13 @@
+from fastapi_health_check.checks import AppAliveCheck, HealthCheck
+from fastapi_health_check.integration.fastapi import install_health_check
+from fastapi_health_check.models import HealthCheckResult, HealthReport
+from fastapi_health_check.registry import HealthRegistry
+
+__all__ = [
+    "AppAliveCheck",
+    "HealthCheck",
+    "HealthCheckResult",
+    "HealthRegistry",
+    "HealthReport",
+    "install_health_check",
+]
